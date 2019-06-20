162ee87c49ba15253ac94f8c7cd37f7ab412ecc5
francisco <femolina@uc.cl>
2019-06-20 12:25:02 +0200

    Makefiles/suit.v4.http.mk: add genkey recipe

--- Makefiles/suit.v4.http.mk
+++ Makefiles/suit.v4.http.mk
@@ -8,7 +8,7 @@ SUIT_OTA_SERVER_URL ?= http://$(SUIT_COAP_SERVER):8080
 SUIT_OTA_SERVER_COAP_URL_EP ?= coap/url
 
 # The OTA server knows where the device can fetch the slots and manifest
-SUIT_COAP_ROOT := $(shell curl -X GET \
+SUIT_COAP_ROOT ?= $(shell curl -X GET \
       $(SUIT_OTA_SERVER_URL)/$(SUIT_OTA_SERVER_COAP_URL_EP)/$(SUIT_PUBLISH_ID))
 
 suit/manifest: $(SUIT_MANIFEST)
@@ -27,3 +27,11 @@ suit/notify:
 		-F 'publish_id=$(SUIT_PUBLISH_ID)' \
 		-F 'urls=$(SUIT_CLIENT)' \
 		$(SUIT_OTA_SERVER_URL)/notifyv4
+
+$(SUIT_KEY) $(SUIT_PUB):
+	@$(RIOTBASE)/dist/tools/suit_v4/gen_key.py
+
+$(SUIT_PUB_HDR): $(SUIT_PUB)
+	@xxd -i $(SUIT_PUB) > $@
+
+suit/genkey: $(SUIT_KEY) $(SUIT_PUB) $(SUIT_PUB_HDR)
