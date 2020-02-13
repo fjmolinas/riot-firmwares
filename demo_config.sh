c19d112f79a0a633541a01bab3d34327dc19d157
Francisco Molina <femolina@uc.cl>
2020-02-13 18:19:35 +0100

    demo_config.sh: remove RIOT_MAKEFILES_GLOBAL_POST

--- demo_config.sh
+++ demo_config.sh
@@ -3,10 +3,6 @@ export LOCAL_IP6_PREFIX="2001:6d6f:6c69:6e61"
 # This should match the URL of the ota-server, so `--http-port`
 # and `--http-host`
 export SUIT_OTA_SERVER_URL="http://127.0.0.1:8888"
-# Overrite suit/notify and suit/publish targets to use ota-server
-# path will be of something like:
-# home/user/riot-firmwares/Makefiles/suit.v4.http.mk
-export RIOT_MAKEFILES_GLOBAL_POST=${SUIT_MAKEFILE_HTTP}
 # If wants to override the application 
 export APPLICATION=air_monitor
 # Address for the SUIT_CLIENT, if the same prefix is used for the
