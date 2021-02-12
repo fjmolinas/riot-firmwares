208218785267c6ed9b22a3179b5874aebb80227a
Francisco Molina <femolina@uc.cl>
2021-02-12 18:54:50 +0100

    *Makefile*: cleanups

--- makefiles/suit.http.mk
+++ makefiles/suit.http.mk
@@ -3,17 +3,19 @@
 SUIT_PUBLISH_ID ?= $(BOARD)_$(APPLICATION)
 
 # Some requirements to publish and notify updates to the server
-SUIT_COAP_SERVER ?= localhost
-SUIT_OTA_SERVER_URL ?= http://$(SUIT_COAP_SERVER):8080
+SUIT_OTA_SERVER_HOST ?= localhost
+SUIT_OTA_SERVER_PORT ?= 8080
+SUIT_OTA_SERVER_BASE_URL ?=
+SUIT_OTA_SERVER_URL ?= http://$(SUIT_OTA_SERVER_HOST):$(SUIT_OTA_SERVER_PORT)$(SUIT_OTA_SERVER_BASE_URL)
 SUIT_OTA_SERVER_COAP_URL_EP ?= coap/url
 
 SUIT_NOTIFY_VERSION ?= latest
 
 # The OTA server knows where the device can fetch the slots and manifest
-SUIT_COAP_ROOT := $(shell curl -X GET \
+SUIT_COAP_ROOT := $(shell curl -s -X GET \
       $(SUIT_OTA_SERVER_URL)/$(SUIT_OTA_SERVER_COAP_URL_EP)/$(SUIT_PUBLISH_ID))
 
-suit/publish: $(SUIT_MANIFEST) $(SUIT_MANIFEST_SIGNED) $(SLOT0_RIOT_BIN) $(SLOT1_RIOT_BIN)
+suit/publish: $(SUIT_MANIFESTS) $(SLOT0_RIOT_BIN) $(SLOT1_RIOT_BIN)
 	$(Q)curl -X POST \
 		-F publish_id=$(SUIT_PUBLISH_ID) \
 		-F $(SUIT_MANIFEST)=@$(SUIT_MANIFEST) \
@@ -27,4 +29,4 @@ suit/notify: | $(filter suit/publish, $(MAKECMDGOALS))
 		-F 'publish_id=$(SUIT_PUBLISH_ID)' \
 		-F 'urls=$(SUIT_CLIENT)' \
 		-F 'version=$(SUIT_NOTIFY_VERSION)' \
-		$(SUIT_OTA_SERVER_URL)/notifyv4
+		$(SUIT_OTA_SERVER_URL)/notify
