19a294f15d196315873fd032320c03c61a6c3c7b
Francisco Molina <femolina@uc.cl>
2021-02-12 18:57:39 +0100

    boards: add external BOARDs dir

---
+++ boards/enviro-nrf52840/board.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2020 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @{
+ *
+ * @file
+ * @author  Martine Lenders <m.lenders@fu-berlin.de>
+ */
+
+#include "cpu.h"
+#include "board.h"
+
+#include "periph/gpio.h"
+
+extern void board_feather_nrf52840_init(void);
+
+void board_init(void)
+{
+    board_feather_nrf52840_init();
+}
+
+/** @} */
