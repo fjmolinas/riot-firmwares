e3a5ad325bedb1bb282257022e7af543e1012727
Francisco Molina <femolina@uc.cl>
2021-03-08 18:04:31 +0100

    gateway: add openwsn border router application

---
+++ gateway/README.md
@@ -0,0 +1,4 @@
+### cdc-ecm Border router
+
+When using cdc-ecm UHCPD is used, make sure that DHCP does not run
+on the cdc-ecm interface.
---
+++ gateway/openwsn_only/main.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2017 Hamburg University of Applied Sciences
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     tests
+ * @{
+ *
+ * @file
+ * @brief       OpenWSN test application
+ *
+ * @author      Peter Kietzmann <peter.kietzmann@haw-hamburg.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    return 0;
+}
